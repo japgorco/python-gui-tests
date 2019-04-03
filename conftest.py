85a8b495748cd37010c312caaf282678d6f5764b
Ivan Shilkin <ivan.shilkin@leotechnology.de>
2019-04-03 16:31:25 +0300

    add setup project

--- conftest.py
+++ conftest.py
@@ -2,7 +2,8 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
+
 def app(request):
     fixture = Application("d:\\Study\\PythonForTesters\\FreeAddressBookPortable\\AddressBook.exe")
     request.addfinalizer(fixture.destroy)
